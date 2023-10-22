f9edc23fba1dd746206c2c346b6d524bf89d1d6f
Fabrizio Milioni <fabrizio.milioni@gmail.com>
2023-10-22 19:04:03 +0200

    update

--- partial/banner.php
+++ partial/banner.php
@@ -1,5 +1,5 @@
 <div class="grid grid-cols-1 lg:grid-cols-3 lg:gap-0 text-center">
-    <div class="bg-[url('/img/banner-play.png')] bg-cover py-20"><h2 class="text-white block">play</h2><button class="btn-special btn-play mt-5">ENJOY THE EXPERIENCE</button></div>
-    <div class="bg-[url('../img/banner-watch.png')] bg-cover py-20"><h2 class="text-white">watch</h2><button class="btn-special btn-watch mt-5">LEARN FROM THE BEST</button></div>
-    <div class="bg-[url('../img/banner-collect.png')] bg-cover py-20"><h2 class="text-white">collect</h2><button class="btn-special btn-collect mt-5">GET YOUR REWARDS</button></div>
+    <div class="bg-[url('../img/banner-play.png')] bg-cover bg-center py-20"><h2 class="text-white block">play</h2><button class="btn-special btn-play mt-5">ENJOY THE EXPERIENCE</button></div>
+    <div class="bg-[url('../img/banner-watch.png')] bg-cover bg-center py-20"><h2 class="text-white">watch</h2><button class="btn-special btn-watch mt-5">LEARN FROM THE BEST</button></div>
+    <div class="bg-[url('../img/banner-collect.png')] bg-cover bg-center py-20"><h2 class="text-white">collect</h2><button class="btn-special btn-collect mt-5">GET YOUR REWARDS</button></div>
 </div>
